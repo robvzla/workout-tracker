131a9f3cf0b0f16b9a81d2431ebb5bafb2e6454c
roberto cassino <robertocassino@student.griffith.ie>
2023-11-18 18:50:06 +0000

    Initial server setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+
+require('dotenv').config()
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+
+const app = express()
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port 4000')
+})
