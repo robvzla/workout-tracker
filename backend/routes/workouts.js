6ac02f0100efb7c6102e043240117c7da5c789b1
roberto cassino <robertocassino@student.griffith.ie>
2023-11-18 20:15:25 +0000

    Adding functions to the routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,26 +1,23 @@
 
 const express = require('express')
+const { 
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+ } = require('../controllers/workoutController')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all workouts'})
-})
+router.get('/', getWorkouts)
 
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET a single workout'})
-})
+router.get('/:id', getWorkout)
 
-router.post('/', (req, res) => {
-    res.json({mssg: 'POST a new workout'})
-})
+router.post('/', createWorkout)
 
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a single workout'})
-})
+router.delete('/:id', deleteWorkout)
 
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a single workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router 
