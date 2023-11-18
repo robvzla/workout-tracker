423f068e7ac1ae531d1b8bc29a904b3b7eeefb5d
roberto cassino <robertocassino@student.griffith.ie>
2023-11-18 19:22:23 +0000

    Updating routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,6 +3,24 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', () => {})
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all workouts'})
+})
 
-module.exports = router
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single workout'})
+})
+
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new workout'})
+})
+
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a single workout'})
+})
+
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE a single workout'})
+})
+
+module.exports = router 
